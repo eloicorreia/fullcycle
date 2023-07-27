9a094ed4f6a0b0c1bfbda59ea421332540250ae1
eloicorreia <eloi.correia@gmail.com>
2023-07-27 03:39:35 +0000

    Segunda entrega

--- src/category/domain/repository/category.repository.ts
+++ src/category/domain/repository/category.repository.ts
@@ -2,7 +2,7 @@ import {
     SearchParams as DefaultSearchParams,
     SearchResult as DefaultSearchResult,
     SearchableRepositoryInterface } from '../../../@seedwork/domain/repository/repository-contracts';
-import { Category } from './../entities/category';
+import { Category } from '../entities/category';
 
 export namespace CategoryRepository {
     export type Filter = string;
